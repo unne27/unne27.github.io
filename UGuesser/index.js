ef3befec699a394064c551aa05edfd0f6357db6d
unne27 <59091348+unne27@users.noreply.github.com>
2022-08-14 19:24:18 +0200

    Update index.js

--- UGuesser/index.js
+++ UGuesser/index.js
@@ -11,7 +11,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
-  });
+  }); 
 }
 */
 var markers = []
