dafe77903d9fa52ebc1cfcd313997eb6cc821955
unne27 <swox.player@gmail.com>
2023-05-17 17:45:20 +0200

    a

--- UGuesser/addLocs/index.js
+++ UGuesser/addLocs/index.js
@@ -19,7 +19,7 @@ function initialize() {
     map.addListener("click", (mapsMouseEvent) => {
      //   navigator.permissions.query({name: "clipboard-write"}).then((result) => {
        //     if (result.state === "granted" || result.state === "prompt") {
-              var newstr = "{lat: " + mapsMouseEvent.latLng.lat + ", lng: " + mapsMouseEvent.latLng.lng + "}"
+              var newstr = "{lat: " + mapsMouseEvent.latLng.lat() + ", lng: " + mapsMouseEvent.latLng.lng() + "}"
               updateClipboard(newstr)
               var marker = new google.maps.Marker({
                 position: mapsMouseEvent.latLng,
