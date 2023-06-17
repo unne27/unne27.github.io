a2a53ce37daa601a839083eb11f2f069d97f4702
unne27 <swox.player@gmail.com>
2023-06-17 12:24:45 +0200

    a

--- UGuesser/game/index.js
+++ UGuesser/game/index.js
@@ -689,6 +689,7 @@ function initialize() {
     clearInterval(interval)
     time = parseInt(timerTime)
     console.log("Timer ended!")
+    consolelog("Interval: " + interval.toString())
     if (document.getElementById("distancemap").style != "display:block;width:100%;height:100%;") {
     
     onbtnclicked()
